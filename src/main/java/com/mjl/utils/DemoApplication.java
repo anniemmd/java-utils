c10f069456901be4be43b6e759fbb4dabe7a2fe4
majiali <majiali@gomo.com>
2018-05-15 10:20:01 +0800

    init project

---
+++ src/main/java/com/mjl/utils/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.mjl.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
