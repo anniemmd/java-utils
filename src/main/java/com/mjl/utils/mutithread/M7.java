74468f0429079faba7e4550f23260d7fc63a4ec2
majiali <majiali@gomo.com>
2018-05-30 22:13:14 +0800

    test

--- src/main/java/com/mjl/utils/mutithread/M7.java
+++ src/main/java/com/mjl/utils/mutithread/M7.java
@@ -2,65 +2,56 @@ package com.mjl.utils.mutithread;
 
 /**
  * Created by majiali on 2018/5/30.
- * 线程通信
+ * 线程通信：
  * 用两个线程分别打印0-100之间的奇偶数。
-     比如 有A，B两个线程；
-     A线程打印1，3，5，7，9 … 99
-     B线程打印0，2，4，6，8 … 100
+ * 比如 有A，B两个线程；
+ * A线程打印1，3，5，7，9 … 99
+ * B线程打印0，2，4，6，8 … 100
  */
 public class M7 {
+    private static Object obj = new Object();
 
     public static void main(String[] args) {
-        Thread a = new Thread(new A());
-        Thread b = new Thread(new B());
+        Thread a = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (M7.obj){
+                for(int i=0; i<100;i+=2){
+                        System.out.println(Thread.currentThread().getName()+":"+i);
+                    try {
+                        notify();
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                }
+            }
+        });
+
+        Thread b = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized(M7.obj){
+                for (int i=1; i<100; i+=2){
+                    try {
+                        System.out.println(Thread.currentThread().getName() +":" + i);
+                        notify();
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                }
+            }
+        });
+
+
         a.start();
         b.start();
+
+
+
     }
 
-    static class A implements Runnable{
-
-        @Override
-        public synchronized void run() {
-            printA();
-        }
-
-        public void printA(){
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            for(int i=0 ; i<100 ; i+=2){
-                System.out.println(Thread.currentThread().getName()+":" + i);
-                notify();
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-    }
-
-    static class B implements Runnable{
-
-        @Override
-        public void run() {
-            printB();
-        }
-
-        public void printB(){
-
-            for(int i=1 ; i<100 ; i+=2){
-                try {
-                    wait();
-                    System.out.println(Thread.currentThread().getName()+":" + i);
-                    notify();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
 }
---
+++ src/main/java/com/mjl/utils/mutithread/M8.java
@@ -0,0 +1,20 @@
+package com.mjl.utils.mutithread;
+
+/**
+ * Created by majiali on 2018/5/30.
+ * 线程通信
+ * 用两个线程分别打印0-100之间的奇偶数。
+     比如 有A，B两个线程；
+     A线程打印1，3，5，7，9 … 99
+     B线程打印0，2，4，6，8 … 100
+ */
+public class M8 {
+    private  Object a = new Object();
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
